a6b67a19d1434b896b184f65c863fb072b205842
Kuthula Langa <jklanga@gmail.com>
2021-05-05 03:38:47 +0200

    Alert messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,6 +81,19 @@
         </nav>
 
         <main class="py-4">
+            <div class="container">
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                @include('notifications')
+            </div>
+
             @yield('content')
         </main>
     </div>
